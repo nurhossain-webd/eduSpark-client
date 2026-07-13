bb9d1d1538cef39185ab09a0460dc865e16e772b
Md Nur Hossain Riyad <md_nur_hossain.riyad@smail.th-koeln.de>
2026-07-14 00:27:05 +0600

    Create Mongoose user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,45 @@
-// TODO
+import { InferSchemaType, Model, Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export type UserDocument = InferSchemaType<typeof userSchema>;
+
+const User: Model<UserDocument> =
+  models.User || model<UserDocument>("User", userSchema);
+
+export default User;
